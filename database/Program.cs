3ac321e91c35bc28545a59d9e6f4e93f539f3358
Adnan <adnanba@gmail.com>
2023-11-22 20:49:01 +0100

    add first migration
    migration

--- database/Program.cs
+++ database/Program.cs
@@ -1,6 +1,14 @@
+using database.context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+ builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DbContext")));
 
 var app = builder.Build();
  
