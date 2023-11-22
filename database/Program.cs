2aba8921f105e158bb6744aafd69ca1306235783
Adnan <adnanba@gmail.com>
2023-11-22 21:04:31 +0100

    add to init

--- database/Program.cs
+++ database/Program.cs
@@ -1,16 +1,17 @@
 using database.context;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-
- builder.Services.AddDbContext<AuthDbContext>(options =>
+// Add services to the container. 
+builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DbContext")));
 
 var app = builder.Build();
- 
+
+var service =  app.Services.GetService<AuthDbContext>();
+
+service?.Database.Migrate(); 
+
 app.Run();
  
